1de6474cfacd9232101617dcc6e9484808d0e1ef
ShehaanKhurram <mshehaankhurram@gmail.com>
2025-08-25 02:37:51 +0500

    Typing_Counter.py

--- Typing_Counter.py
+++ Typing_Counter.py
@@ -10,27 +10,36 @@ label1 = tk.Label(root,text="Enter Something",font=("Arial",16), bg="lightyellow
 label1.pack(pady=30)
 
 
-textbox1 = tk.Text(root,height=10,width=25)
+textbox1 = tk.Text(root,height=10,width=30, wrap="word")
 textbox1.pack(pady=5)
 
+def word_count():
+    user_text = textbox1.get("1.0","end-1c").strip() 
+    if not user_text:
+        return 0
+    return len(user_text.split())
+
 def count(event=None):
-    count = len(textbox1.get("1.0","end-1c"))
-    counter_label.config(text=f"Characters: {count}")
+    w_count = word_count()
+    ch_count = len(textbox1.get("1.0","end-1c"))
+    words_label.config(text=f"words: {w_count}")
+    counter_label.config(text=f"Characters: {ch_count}")
 
 textbox1.bind("<KeyRelease>", count)
 
 bottom_frame = tk.Frame(root, bg="lightyellow")
 bottom_frame.pack(side="bottom",fill="x")
 
+words_label = tk.Label(bottom_frame,text="Words: 0", font=("Arial", 14), bg="lightyellow")
+words_label.pack(side="top",pady=5)
+
 counter_label = tk.Label(bottom_frame,text="Characters: 0", font=("Arial", 14), bg="lightyellow")
 counter_label.pack(side="top",pady=5)
 
-# Exit Button
 def exit_function():
     root.destroy()
 
 exit_button = tk.Button(bottom_frame, text="Exit", command=exit_function, bg="green", fg="white", width=10,font=("Arial", 10))
 exit_button.pack(side="bottom",pady=20)
-
 
 root.mainloop()
